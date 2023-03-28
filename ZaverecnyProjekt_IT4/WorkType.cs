3ce1a5845175462c3e45f8a0507f2434c59c49b4
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2023-03-28 23:40:35 +0200

    Vytvoření tabulky a třídy WorkType

---
+++ ZaverecnyProjekt_IT4/WorkType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaverecnyProjekt_IT4
+{
+    public class WorkType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public WorkType(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
