8a091bcf582f9ed856c25bef3b7c24a09de8ff73
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2023-03-28 23:14:15 +0200

    Třída Contract

---
+++ ZaverecnyProjekt_IT4/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaverecnyProjekt_IT4
+{
+    public class Contract
+    {
+        public int ID { get; set; }
+        public string Customer { get; set; }
+        public string Description { get; set; }
+
+        public Contract(string customer, string description)
+        {
+            Customer = customer;
+            Description = description;
+        }
+    }
+}
