37bb9fe4312898fbec56ef378dacf12ca678e020
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2023-03-30 18:36:25 +0200

    Uložení query pro tabulu user a role

---
+++ Databáze + tabulky/role-table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Role] (
+    [id]      INT           IDENTITY (1, 1) NOT NULL,
+    [name]    NVARCHAR (50) NOT NULL,
+    [isadmin] BIT           DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    UNIQUE NONCLUSTERED ([name] ASC)
+);
---
+++ Databáze + tabulky/user-table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]           INT              IDENTITY (1, 1) NOT NULL,
+    [name]         NVARCHAR (50)    NOT NULL,
+    [passwordhash] VARBINARY (1024) NOT NULL,
+    [passwordsalt] VARBINARY (1024) NOT NULL,
+    [role]         INT              DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([role]) REFERENCES [dbo].[Role] ([id])
+);
